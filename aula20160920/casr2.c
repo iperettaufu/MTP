b146e68eecf3084a8f90a7e8c3d3419e78dddaf0
iperettaufu <iperetta@ufu.br>
2016-09-22 12:28:58 -0300

    Update casr2.c

--- aula20160920/casr2.c
+++ aula20160920/casr2.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
-#include <math.h>
 #include <ctype.h>
-
-void recebe_dado(char tipo, void * endereco);
-
+#include <string.h>
+#include <math.h>
+void recebe(char tipo, void * endereco);
+void recebeSTR(char * endereco, int n);
 int main() {
-    int i; float f; char c;
-    printf("Entre com um inteiro: "); recebe_dado('i',&i);
-    printf("Entre com um float: "); recebe_dado('f',&f);
-    printf("Entre com um caractere: "); recebe_dado('c',&c);
-    printf("Quadrado: %d\n", i*i);
-    printf("Raiz: %f\n", sqrt(f));
-    printf("Caractere: %c\n", (isupper(c))? tolower(c) : toupper(c));
+    int i; float f; char c; char s[256];
+    printf("Entre com um inteiro: ");   recebe('i', &i);
+    printf("Entre com um float: ");     recebe('f', &f);
+    printf("Entre com um caractere: "); recebe('c', &c);
+    printf("Entre com uma frase: ");    recebeSTR(s, sizeof(s));
+    printf("Para %d, o quadrado: %d\n", i, i*i);
+    printf("Para %f, a raiz: %f\n", f, sqrt(f));
+    printf("Para %c, o caractere: %c\n", c, (islower(c)) ? toupper(c) : tolower(c));
+    printf("A frase '%s' tem %d caracteres\n", s, strlen(s));
     return 0;
 }
-
-void recebe_dado(char tipo, void * endereco, int n = 0) {
+void recebe(char tipo, void * endereco) {
     int c;
-    switch(tipo) {
-    case 'i': scanf("%d",(int *)endereco); break;
-    case 'f': scanf("%f",(float *)endereco); break;
-    case 'c': scanf("%c",(char *)endereco); break;
-    case 's': fgets((char *)endereco, n, stdin); break;
+    switch(tipo){
+        case 'i': scanf("%d",(int*) endereco); break;
+        case 'f': scanf("%f",(float*) endereco); break;
+        case 'd': scanf("%lf",(double*) endereco); break;
+        case 'c': scanf("%c",(char*) endereco); break;
     }
-    while((c = getchar()) != EOF && c != '\n'); //limpar buffer
+    while((c = getchar()) != EOF && c != '\n'); // apagar o buffer de entrada
+}
+
+void recebeSTR(char * endereco, int n) {
+    fgets(endereco, n, stdin);
+    if(endereco[strlen(endereco)-1] == 10)
+       endereco[strlen(endereco)-1] = '\0';
 }
