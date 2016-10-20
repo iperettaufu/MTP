88d9cda9b8ce282fea522c05f9e2cd06d6dd99e4
Your Name <you@example.com>
2016-10-20 12:36:04 -0200

    test OEM 850 b

---
+++ aula20161018/testOEM850_b.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#if (defined __WIN32__ || defined __WIN64__)
+	#include <windows.h>
+	#undefine printf
+	#define printf winprintf
+#endif
+
+char * winprintf(char * texto);
+
+int main(){
+    printf("::: Com PRINTF :::\n");
+    printf("AtenÁ„o: teste 1, 2, 3\nÁ„È‡«’ \n");
+    printf("::: Com WINPRINTF :::\n");
+    printf("AtenÁ„o: teste %d, 2, 3\nÁ„È‡«’ \n", 1);
+    return 0;
+}
+
+char * winprintf(char * texto) {
+    char * aux;
+    int i, len = 0;
+    while(texto[len]) len++;
+    aux = (char *) calloc(len + 1, sizeof(char));
+    for(i = 0; i <= len; i++) aux[i] = texto[i];
+    CharToOem(aux, aux);
+    fprintf(stdout, "%s", aux);
+    free(aux);
+}
